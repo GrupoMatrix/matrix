9a8033575834c95a5c6a8e08bc649566083914db
Matheus Kaussinis <matheus@adalov.com>
2021-09-17 19:53:12 -0300

    Alterando a home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,16 +72,6 @@
                 <div class="title m-b-md" >
                    Hello Word Matrix!
                 </div>
-                @if (Route::has('login'))
-                    <div class="botton-right links">
-                        @auth
-                            <a href="{{ url('/home') }}">Home</a>
-                        @else
-                            <a href="{{ route('login') }}">Login</a>
-                            <a href="{{ route('register') }}">Register</a>
-                        @endauth
-                    </div>
-                @endif
             </div>
         </div>
     </body>
