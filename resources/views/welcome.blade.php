8ba90f19ad00783843100f5dee76aa132e234af4
Matheus Kaussinis <matheus@adalov.com>
2021-09-16 22:51:52 -0300

    Nova alteração na home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PedidoCerto</title>
+        <title>Matrix</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -72,17 +72,6 @@
                 <div class="title m-b-md" >
                    Hello Word!
                 </div>
-            @if (Route::has('login'))
-                <div class="botton-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             </div>
         </div>
     </body>
